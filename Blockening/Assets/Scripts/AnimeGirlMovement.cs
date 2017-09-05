e4f7123610dffbc52fbedc7fb990499b9aaeef29
AIE\s171102 <alpi1997@hotmail.com>
2017-09-05 16:54:02 +1000

    Game with character movement - not finished yet, models for blocks are done.

---
+++ Blockening/Assets/Scripts/AnimeGirlMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimeGirlMovement : MonoBehaviour
+{
+	public float m_fSpeed = 10.0f;
+	private Animator anim;
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		float H = Input.GetAxis("Horizontal") * Time.deltaTime * m_fSpeed;
+		float V = Input.GetAxis("Vertical") * Time.deltaTime * m_fSpeed;
+
+		if (H < 0)
+		{
+			anim.SetBool("IsLeft", true);
+		}
+		else if(H > 0)
+		{
+			anim.SetBool("IsRight", true);
+		}
+		else
+		{
+			anim.SetBool("IsLeft", false);
+			anim.SetBool("IsRight", false);
+		}
+
+		if(V != 0)
+		{
+			anim.SetTrigger("IsJumping");
+		}
+
+		transform.Translate(H, 0, 0);
+
+		transform.Translate(0, V, 0);
+
+
+	}
+
+
+}
